bc5b8be1dd962d9b1175593245da3cfbae8e140b
jeremyliu622 <bliu90@my.bcit.ca>
2020-10-09 17:32:20 -0700

    add order functions
    submit order, check order history

--- src/app/app.menu.ts
+++ src/app/app.menu.ts
@@ -14,7 +14,13 @@ export class MenuComponent {
     _foodGroupDict;
     selectedFood;
     orderedFood: Object = {};
+    unCompletedOrders;
+    historyOrder: Object = {};
+    historyPrice: number = 0;
+    showHistory: Boolean = false;
     totalPrice: number = 0;
+    tableNum: number;
+    _errorMessage: String = "";
 
 
 
@@ -95,5 +101,74 @@ export class MenuComponent {
 
     }
 
+    submitOrder() {
+        let url = this._url + "submitOrder/" + this.tableNum;
+        if(!this.tableNum) {
+            this._errorMessage = "Please enter a table number."
+        }
+        else if(this.totalPrice == 0) {
+            this._errorMessage = "Please select at least one item."
+        }
+        else {
+        let newItem = {
+            "tableNum": this.tableNum,
+            "totalPrice": this.totalPrice,
+            "orderItems": this.orderedFood,
+            "isCompleted": false,
+        }
+        this._http.post<any>(url, newItem)
+        .subscribe(data =>{
+            this._errorMessage = data.errorMessage;
+            this.clearOrder();
+        })
+        if(this._errorMessage == "") {
+            alert("The order has been placed.")
+
+            } 
+        }
+    }
+
+    clearOrder() {
+        this.orderedFood = {};
+        this.totalPrice = 0;
+        this._errorMessage = "";
+        this.showHistory = false;
+    }
+
+    orderHistory() {
+        let url = this._url + "order/" + this.tableNum;
+
+        this._http.get<any>(url)
+            .subscribe(result => {
+                this.unCompletedOrders = result.unCompletedOrders;
+                this.combineUnCompletedOrders();
+                this._errorMessage = result._errorMessage;
+                if (!this.unCompletedOrders || this.unCompletedOrders.length == 0) {
+                    this._errorMessage = "No order history."
+                } else{
+                    if(this.showHistory){
+                        this.showHistory = false;
+                    }
+                    else{
+                        this.showHistory = true;
+                    }
+                }
+            })
+
+    }
+
+    combineUnCompletedOrders() {
+        this.historyOrder = {};
+        this.historyPrice = 0;
+        for(let i in this.unCompletedOrders) {
+            this.historyPrice += parseFloat(this.unCompletedOrders[i].totalPrice);
+            for(let key in this.unCompletedOrders[i].orderItems[0]){
+                if(!this.historyOrder[key]) {
+                    this.historyOrder[key] = 0;
+                }
+                this.historyOrder[key] += this.unCompletedOrders[i].orderItems[0][key];
+            }
+        }  
+    }
 
 }
